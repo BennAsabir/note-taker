af18653e93d6dc5b4168f89e4a69c5d301280b45
Benzino <benasabir@gmail.com>
2021-02-20 20:51:15 -0500

    created routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+let savedNote = [];
+
+fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    savedNote = (JSON.parse(data));
+});
+
+editNote = () => {
+    fs.writeFile("./db/db.json", JSON.stringify(savedNote), (err) => {
+        if (err) throw err;
+    });
+}
+
+
+
+module.exports = function (app) {
+// GET
+    app.get("/api/notes", (req, res) => {
+        res.json(savedNote);
+      });
+// POST
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body
+        newNote.id = (uuidv4());
+        savedNote.push(newNote);
+        editNote();
+        res.send(savedNote);
+    });
+// DELETE
+    app.delete("/api/notes/:id", (req, res) => {
+        idDelete = req.params.id
+        for (let i = 0; i < savedNote.length; i++) {
+            if (savedNote[i].id === idDelete) {
+                savedNote.splice(i, 1);
+                editNote();
+                res.send(savedNote);
+                return;
+            }
+        }
+    });
+}
